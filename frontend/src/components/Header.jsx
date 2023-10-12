2e73a146fa35ff97c705e12e3114df8336ef4213
Miles Eng <miles.eng@duke.edu>
2023-10-12 12:48:33 -0400

    updated styling for product screen page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,16 +6,16 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa'
 export const Header = () => {
   return (
     <header>
-      <div className='w-full flex flex-row justify-between px-4 md:px-8 py-4 bg-forest border-b-2 border-forest-900 text-cloud-50'>
+      <div className='w-screen flex flex-row justify-between px-4 md:px-8 py-4 bg-forest border-b-2 border-forest-900 text-default-bg'>
         <Link to='/' className='text-2xl'>WolfShop</Link>
         <div className='flex flex-row gap-4 md:gap-8 align-middle'>
           <Link to='/cart'
             className='flex flex-row gap-2 my-auto'>
-            <FaShoppingCart className='my-auto' /> Cart
+            <FaShoppingCart className='my-auto mx-4 md:mx-0' /> <span className='hidden md:block'>Cart</span>
           </Link>
           <Link to='/login'
             className='flex flex-row gap-2 my-auto'>
-            <FaUser className='my-auto' /> Log in
+            <FaUser className='my-auto mx-4 md:mx-0' /> <span className='hidden md:block'>Log in</span>
           </Link>
         </div>
       </div>
